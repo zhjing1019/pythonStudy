0b80c0c46129d1fa8f624e383f2ffb4d60c5e697
张静 <zhjing1019@gmail.com>
2019-11-12 10:50:46 +0800

    python

--- baseOne.py
+++ baseOne.py
@@ -1,28 +1,28 @@
-# # 单行定义多个变量或赋值
-# a, b = 1, 2
+# 单行定义多个变量或赋值
+a, b = 1, 2
 
-# # 交换俩个值的数值
-# a, b = b, a
+# 交换俩个值的数值
+a, b = b, a
 
-# # 单行定义多个变量或赋值可用于元组拆分
-# data = ('test1', 'test2', 'test3')
-# nameOne, nameTwo, nameThree = data
-# print(nameOne)
-# # test1
-# print(nameTwo)
-# # test2
-# print(nameThree)
-# # test3
+# 单行定义多个变量或赋值可用于元组拆分
+data = ('test1', 'test2', 'test3')
+nameOne, nameTwo, nameThree = data
+print(nameOne)
+# test1
+print(nameTwo)
+# test2
+print(nameThree)
+# test3
 
 
-# # 计算一位数码相机销售人员的工资。他的基本工资是 1500，每售出一台相机他可以得到 200 并且获得 2% 的抽成。程序要求输入相机数量及单价。
-# base_salary = 1500
-# get_rate = 200
-# precent_rate = 0.02
-# count = int(input("请输入相机的数量"))
-# price= int(input("请输入相机的单价"))
-# all_salary = base_salary + get_rate * count + precent_rate * count * price
-# print(all_salary)
+# 计算一位数码相机销售人员的工资。他的基本工资是 1500，每售出一台相机他可以得到 200 并且获得 2% 的抽成。程序要求输入相机数量及单价。
+base_salary = 1500
+get_rate = 200
+precent_rate = 0.02
+count = int(input("请输入相机的数量"))
+price= int(input("请输入相机的单价"))
+all_salary = base_salary + get_rate * count + precent_rate * count * price
+print(all_salary)
 
 
 #if elif else 的基本语法结构。
